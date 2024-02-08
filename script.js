bc5dcf418f70bb304d08eca71825faa3fc5b9efd
debjitnazirit <debjitnazirit@gmail.com>
2024-02-08 11:18:39 +0530

    chandd

--- script.js
+++ script.js
@@ -25,3 +25,24 @@ var swiper = new Swiper(".home-slider",{
     },
     loop:true,
 });
+
+
+var MainImg = document.getElementById("MainImg");
+var smallImg = document.getElementsByClassName("small-img");
+smallImg[0].onclick = function(){
+    MainImg.src = smallImg[0].src;
+}
+
+smallImg[1].onclick = function(){
+    MainImg.src = smallImg[1].src;
+}
+
+smallImg[2].onclick = function(){
+    MainImg.src = smallImg[2].src;
+}
+
+smallImg[3].onclick = function(){
+    MainImg.src = smallImg[3].src;
+}
+
+        
